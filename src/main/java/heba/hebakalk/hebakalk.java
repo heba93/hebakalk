c75e6cbbc8e586b694d642bf5650b50681cac5f0
Heba <heba@Hebas-MacBook-Pro.local>
2016-04-20 15:13:48 +0200

    My Scond Commit

---
+++ src/main/java/heba/hebakalk/hebakalk.java
@@ -0,0 +1,20 @@
+package heba.hebakalk;
+
+public class hebakalk {
+ 
+ public double add(double x, double y){
+    return x+y;
+ }
+ public double sub (double x, double y){
+  
+  return x - y;
+ }
+ public double mul(double x, double y){
+  return x*y;
+ }
+ public double div(double x, double y){
+  return x/y;
+ }
+ 
+
+}
---
+++ src/main/java/heba/hebakalk/main.java
@@ -0,0 +1,18 @@
+package heba.hebakalk;
+
+
+public class main {
+
+	 public static void main(String[] args) {
+	  // TODO Auto-generated method stub
+
+	  hebakalk Calculator = new hebakalk();
+	  
+	  System.out.println( Calculator.add( 15.0, 10.0));
+	  System.out.println( Calculator.sub(150.0, 100.0));
+	  System.out.println( Calculator.mul(1.0, 10.0));
+	  System.out.println( Calculator.div(100.0, 2.0));
+	  
+	 }
+
+	}
